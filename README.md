882c47f2f5e02915e061f0a2714ef980609a8a2d
wsielyfeng <learnroad@163.com>
2021-11-17 22:02:10 +0800

    增加笔记

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 代码为饥人谷第86课静态服务器教学中跟着敲的。以此记录。
 
 2021.11.16
+
+## 创建项目
+
++ yarn init -y
++ 新建index.ts
++ ts-node-dev启动项目
++ 安装 @types/node模块
++ 在index.ts中创建server
++ 监听server的request事件，监听8888端口
+
+## 使用TypeScript
+
++ 参数加上类型，ts就可以提示该参数可用的属性
+  
+## curl命令
+
++ `curl -v http://localhost:8888/`会在命令行中显示请求信息
++ `curl -v -d "name:xxx" http://localhost:8888/` 以post形式去发送请求
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import * as http from 'http'
+import { IncomingMessage, ServerResponse } from 'http'
 
 const server = http.createServer()
 
-server.on('request', (request, response) => {
-    console.log('有人请求了');
+// 通过指定参数类型IncomingMessage，ts才知道有哪些属性可用
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    console.log(request.httpVersion);
+    console.log(request.url);
     response.end('hi');
-    
+
 })
 
 server.listen(8888)
