8a2cd34b050cbc8861ac0d6a61d3c29a15d8e6c9
wsielyfeng <learnroad@163.com>
2021-11-21 20:25:34 +0800

    实现解析url参数

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import * as http from 'http';
 import { IncomingMessage, ServerResponse }  from 'http';
 import * as fs from 'fs';
 import * as p from 'path';
+import * as url from 'url';
 
 const server = http.createServer()
 const publicDir = p.resolve(__dirname, 'public')
 
 server.on('request', (request: IncomingMessage, response: ServerResponse)=>{
-    const {method, url, headers} = request;
-    switch (url){
+    const {method, url: path, headers} = request;
+    const {pathname, search} = url.parse(path); // 解析url参数
+    switch (pathname){  // 对url的响应
         case '/index.html':
             fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) =>{
                 if(error) throw error;
